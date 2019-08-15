a0bc5c362e838c098ee42c21c1e9ce0ea5337879
Diego Salazar Arp <d.salazararp@gmail.com>
2019-08-15 00:01:47 -0400

    Initial Commit

---
+++ 28-ObservableObjectExample/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  28-ObservableObjectExample
+//
+//  Created by Diego Salazar Arp on 15-08-19.
+//  Copyright © 2019 Diego Salazar Arp. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
